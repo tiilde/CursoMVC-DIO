20e776607629561a0c2abea8766ccc55897e1776
Tilde Bastos <tilde.bastos@gmail.com>
2021-12-27 14:27:16 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC-DIO/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC_DIO.Models {
+    // herda do Entity framework DbContext
+    public class Context : DbContext{
+        // método responsável por configurar o Entity Framework
+
+        // tabela categorias
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            // definindo banco de dados
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=CursoMVC_DIO;Integrated Security=True");
+
+        }
+    }
+}
